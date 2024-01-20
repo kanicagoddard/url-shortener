99506842edf826cd8b99bec0e021dab6f7fe77b5
kanicagoddard.kanica <git@github.com:kanicagoddard>
2024-01-20 21:31:01 +0000

    implement health check

---
+++ internal/handlers/healthcheck.go
@@ -0,0 +1,14 @@
+package handlers
+
+import "net/http"
+
+type HealthCheckHandler struct {
+}
+
+func NewHealthHandler() *HealthCheckHandler {
+	return &HealthCheckHandler{}
+}
+
+func (h *HealthCheckHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("OK"))
+}
