e0e728d1200155069bbc946581a96cac005c07a1
kanicagoddard.kanica <kanicagoddard@gmail.com>
2024-01-25 15:36:15 +0000

    add datastore

--- store/dbstore/shorturl.go
+++ store/dbstore/shorturl.go
@@ -5,8 +5,8 @@ import (
 	"url-shortener/store"
 )
 
-type ShortURLSTore struct {
-	shortURLs []store.ShortURL
+type ShortURLStore struct {
+	shortURLs []store.ShortURL // imports from store.gox
 	logger    *slog.Logger
 }
 
@@ -15,11 +15,40 @@ type NewShortURLStoreParams struct {
 	Logger *slog.Logger
 }
 
-func NewShortURLStore(params NewShortURLStoreParams) *ShortURLSTore {
+func NewShortURLStore(params NewShortURLStoreParams) *ShortURLStore {
 	shortURLs := []store.ShortURL{}
 
-	return &ShortURLSTore{
+	return &ShortURLStore{
 		shortURLs: shortURLs,
 		logger:    params.Logger,
 	}
+}
+
+// create shortURL
+func (s *ShortURLStore) CreateShortURL(params store.CreateShortURLParams) (store.ShortURL, error) {
+	shortURL := store.ShortURL{
+		Destination: params.Destination,
+		Slug:        params.Slug,
+		ID:          len(s.shortURLs), //returns a link to the slice using len()
+	}
+
+	s.shortURLs = append(s.shortURLs, shortURL)
+
+	s.logger.Info("short URL created", slog.Any("values -> ", shortURL))
+	return shortURL, nil
+
+}
+
+// get a short URL by slug
+func (s *ShortURLStore) GetShortURLBySlug(slug string) (*store.ShortURL, error) {
+	// use a for loop to search through the shortURLs, if found, return results or error
+
+	for _, shortURL := range s.shortURLs {
+		if shortURL.Slug == slug {
+			result := shortURL
+			return &result, nil
+		}
+	}
+
+	return nil, store.ErrShortURLNotFound
 }
--- store/store.go
+++ store/store.go
@@ -1,7 +1,23 @@
 package store
 
+import "errors"
+
+// url structure
 type ShortURL struct {
-	ID          int    `json:"id"`
-	Destination string `json:"destination"`
-	Slug        string `json:"slug"`
+	ID          int    `json:"id" bson:"shortURLID"`
+	Destination string `json:"destination" bson:"dest"`
+	Slug        string `json:"slug" bson:"slug"`
+}
+
+type CreateShortURLParams struct {
+	Destination string
+	Slug        string
+}
+
+// custom error
+var ErrShortURLNotFound = errors.New("short URL not found")
+
+type ShortURLStore interface {
+	CreateShortURL(params CreateShortURLParams) (ShortURL, error)
+	GetShortURLBySlug(slug string) (*ShortURL, error)
 }
